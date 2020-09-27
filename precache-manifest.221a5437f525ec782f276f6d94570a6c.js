0b519ee901cf3e3e516d1a5d04469ab5fb73020f
Hope21x <officialhopek42@gmail.com>
2020-09-27 18:25:25 +0000

    Updates

---
+++ precache-manifest.221a5437f525ec782f276f6d94570a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019a3e5d369f02de7f609aab747e82d6",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "6c7978b53bd33b8ef669",
+    "url": "/first-app/static/css/main.7ee44a4e.chunk.css"
+  },
+  {
+    "revision": "17e7a3acfa6de3ea2ab4",
+    "url": "/first-app/static/js/2.faf01414.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-app/static/js/2.faf01414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7978b53bd33b8ef669",
+    "url": "/first-app/static/js/main.fea442cc.chunk.js"
+  },
+  {
+    "revision": "3089aeabcfc490fdc7dc",
+    "url": "/first-app/static/js/runtime-main.652d5778.js"
+  }
+]);
